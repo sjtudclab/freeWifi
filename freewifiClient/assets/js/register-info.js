b4634a0afab435dced08c604c6c1edc6623f45ba
ernestyj <ernestyj@outlook.com>
2015-05-30 13:00:24 +0800

    clean old

--- freewifiClient/assets/js/register-info.js
+++
@@ -1,9 +0,0 @@
-$('.dropdown-toggle').dropdown();
-
-$('#ul_education li').click(function() {
-	$('#btn_education').html(this.children[0].text+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="caret"></span>');
-});
-
-$('#ul_income li').click(function() {
-	$('#btn_income').html(this.children[0].text+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="caret"></span>');
-});
--- freewifiClient/gen/cn/edu/sjtu/dclab/freewifi/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package cn.edu.sjtu.dclab.freewifi;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
