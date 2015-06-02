41ff1a836ccd33bed1465b4d222307f970c6a6e5
Kaffa-MY <meng-yang@foxmail.com>
2015-06-02 10:29:58 +0800

    完善广告显示功能

--- freewifiClient/src/cn/edu/sjtu/dclab/freewifi/push/MsgShowAty.java
+++ freewifiClient/src/cn/edu/sjtu/dclab/freewifi/push/MsgShowAty.java
@@ -3,26 +3,45 @@ package cn.edu.sjtu.dclab.freewifi.push;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.view.ViewGroup.LayoutParams;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import cn.edu.sjtu.dclab.freewifi.R;
 import cn.jpush.android.api.JPushInterface;
 
 /**
  * Created by Yang on 2015/5/29.
  */
 public class MsgShowAty extends Activity {
+    private WebView webView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("用户自定义打开的Activity");
+        setContentView(R.layout.aty_adweb);
+
+        webView = (WebView) findViewById(R.id.adweb);
+        webView.getSettings().setJavaScriptEnabled(true);
+
         Intent intent = getIntent();
         if (null != intent) {
             Bundle bundle = getIntent().getExtras();
-            String title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
-            String content = bundle.getString(JPushInterface.EXTRA_ALERT);
-            tv.setText("Title : " + title + "  " + "Content : " + content);
+//            String title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
+//            String content = bundle.getString(JPushInterface.EXTRA_ALERT);
+            String urlJson = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            String urlLink = urlJson.substring(urlJson.indexOf(":") + 2, urlJson.length() - 2);
+
+            webView.loadUrl("http://" + urlLink);
+            webView.setWebViewClient(new AdWebViewClient());
         }
-        addContentView(tv, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     }
+
+    private class AdWebViewClient extends WebViewClient {
+        //重写shouldOverrideUrlLoading方法，使点击链接后不使用其他的浏览器打开
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
 }
