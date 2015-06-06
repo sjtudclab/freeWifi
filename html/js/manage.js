a564b7812b0adc11980c6c6eebd824ef76d49277
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2015-06-06 21:51:31 +0800

    [Function Manage] Fix bugs

--- html/js/manage.js
+++ html/js/manage.js
@@ -1,4 +1,85 @@
 $(function() {
+    function createPublish(i) {
+        var aPublish = document.createElement('a');
+        $(aPublish).addClass('btn btn-success btn-mini');
+        $(aPublish).text('Publish');
+        $(aPublish).click(function(){
+            $.ajax({
+                url: "ad/publish",
+                method: 'GET',
+                data: {id:i},
+            }).done(function( data ) {
+                if (data.code == -1) {
+                    console.log('error');
+                } else {
+                    console.log('succeed');
+                }
+            });
+        });
+        return aPublish;
+    }
+
+    function createUnpublish(i) {
+        var aUnpublish = document.createElement('a');
+        $(aUnpublish).addClass('btn btn-success btn-mini');
+        $(aUnpublish).text('Unpublish');
+        $(aUnpublish).click(function(){
+            $.ajax({
+                url: "ad/unpublish",
+                method: 'GET',
+                data: {id: i},
+            }).done(function( data ) {
+                if (data.code == -1) {
+                    console.log('error');
+                } else {
+                    console.log('succeed');
+                }
+            })                        
+        });
+        return aUnpublish;
+    }
+    
+    function createPreview(i) {
+        var aPreview = document.createElement('a');
+        $(aPreview).addClass('btn btn-success btn-mini');
+        $(aPreview).text('Preview');
+        $(aPreview).click(function(){
+            location.href = document.location.hostname + '/ad/mobile?id=' + i;
+        });
+        return aPreview;
+    }
+
+    function createReport(i) {
+        var aReport = document.createElement('a');
+        $(aReport).addClass('btn btn-danger btn-mini');
+        $(aReport).text('Report');
+        $(aReport).click(function(){
+            location.href = '/freewifiserver/ad/stats?id=' + i;
+        });
+        return aReport;
+    }
+
+    function createDelete(i) {
+        var aDelete = document.createElement('a');
+        $(aDelete).addClass('btn btn-danger btn-mini');
+        $(aDelete).text('Delete');
+        $(aDelete).click(function() {
+            $.ajax({
+                url: "ad/delete",
+                method: 'GET',
+                data: {id: i},
+            }).done(function( data ) {
+                if (data.code == -1) {
+                    console.log('error');
+                } else {
+                    $('#recent-posts').empty();
+                    getADs();
+                }
+            })                 
+        });
+        return aDelete;
+    }
+
     function getADs() {
         $.ajax({
             url: "ad/get",
@@ -28,76 +109,17 @@ $(function() {
                     endHour = endHour.substr(0, 2) + ':' + endHour.substr(2, 4) +
                         ':' + endHour.substr(4, 6);
                     $(a).text(ad.startDate + '--' + ad.endDate + '    ' + startHour + '--' + endHour);
-                    var aEdit = document.createElement('a');
-                    $(aEdit).addClass('btn btn-primary btn-mini');
-                    $(aEdit).text('Edit');
-                    $(aEdit).click(function(){
-                        location.href = document.location.hostname + '?id=' + ad.id;
-                    });
-                    var aPublish = document.createElement('a');
-                    $(aPublish).addClass('btn btn-success btn-mini');
-                    $(aPublish).text('Publish');
-                    $(aPublish).click(function(){
-                        $.ajax({
-                            url: "ad/publish",
-                            method: 'GET',
-                            data: {id:ad.id},
-                        }).done(function( data ) {
-                            if (data.code == -1) {
-                                console.log('error');
-                            } else {
-                                console.log('succeed');
-                            }
-                        });
-                    });
-                    var aUnpublish = document.createElement('a');
-                    $(aUnpublish).addClass('btn btn-success btn-mini');
-                    $(aUnpublish).text('Unpublish');
-                    $(aUnpublish).click(function(){
-                        $.ajax({
-                            url: "ad/unpublish",
-                            method: 'GET',
-                            data: {id:ad.id},
-                        }).done(function( data ) {
-                            if (data.code == -1) {
-                                console.log('error');
-                            } else {
-                                console.log('succeed');
-                            }
-                        })                        
-                    });
-                    var aPreview = document.createElement('a');
-                    $(aPreview).addClass('btn btn-success btn-mini');
-                    $(aPreview).text('Preview');
-                    $(aPreview).click(function(){
-                        location.href = document.location.hostname + '/ad/mobile?id=' + ad.id; 
-                    });
-
-                    var aReport = document.createElement('a');
-                    $(aReport).addClass('btn btn-danger btn-mini');
-                    $(aReport).text('Delete');
-                    $(aReport).click(function(){
-                        location.href = '/freewifiserver/ad/stats?id=' + ad.id;
-                    });
-                    
-
-                    var aDelete = document.createElement('a');
-                    $(aDelete).addClass('btn btn-danger btn-mini');
-                    $(aDelete).text('Delete');
-                    $(aDelete).click(function() {
-                        $.ajax({
-                            url: "ad/delete",
-                            method: 'GET',
-                            data: {id: ad.id},
-                        }).done(function( data ) {
-                            if (data.code == -1) {
-                                console.log('error');
-                            } else {
-                                $('#recent-posts').empty();
-                                getADs();
-                            }
-                        })                 
-                    });
+                    // var aEdit = document.createElement('a');
+                    // $(aEdit).addClass('btn btn-primary btn-mini');
+                    // $(aEdit).text('Edit');
+                    // $(aEdit).click(function(){
+                    //     location.href = document.location.hostname + '?id=' + ad.id;
+                    // });
+                    var aPublish = createPublish(ad.id);
+                    var aUnpublish = createUnpublish(ad.id);
+                    var aPreview = createPreview(ad.id);
+                    var aReport = createReport(ad.id);
+                    var aDelete = createDelete(ad.id);
                     $(li).append(div);
                     $(div).append(span);
                     $(div).append(p);
@@ -105,7 +127,7 @@ $(function() {
                     $(p).append(a);
                     
                     $('#recent-posts').append(li);
-                    $(div).append(aEdit);
+                    // $(div).append(aEdit);
                     $(div).append(aPublish);
                     $(div).append(aUnpublish);
                     $(div).append(aPreview);
