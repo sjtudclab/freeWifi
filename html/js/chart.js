00511087c676157370b1fb3d9172dac6035a0987
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2015-06-06 20:46:38 +0800

    [Function Prototype] Report

---
+++ html/js/chart.js
@@ -0,0 +1,38 @@
+$(function() {
+    var id = getUrlParameter('id');
+    var colors = ["220,220,220", "151,187,205", "151,111,147", "151,111,52", "151,169,52", "23,169,52"];
+
+    function report() {
+        var type = $( "#show option:selected" ).text();
+        $.ajax({
+            url: "ad/stats/" + id,
+            method: 'GET',
+            data: {type: type},
+        }).done(function(re) {
+            var ctx = $("#myChart").get(0).getContext("2d");
+            var data = {};
+            var labels = [];
+            var datasets = [];
+            for (var i in re) {
+                var item = re[i];
+                labels.push(item.orientation)
+                var obj = {};
+                obj.fillColor = 'rgba(' + colors[i] + ', 0.5)';
+                obj.strokeColor = 'rgba(' + colors[i] + ', 1)';
+	        obj.data = [item.click, item.push, item.impression];
+                datasets.push(obj);
+                
+            }
+            data.labels = labels;
+            data.datasets = datasets;
+            var myNewChart = new Chart(ctx).Bar(data);
+        })
+    }
+
+    $('#show').change(function() {
+        report();
+    });
+    if (id) {
+        report();
+    }
+});
